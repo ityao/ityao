f2cf478e307eb4548332eff9d9d5cb5e48c63a73
Silver Lao <silver.lao@gmail.com>
2011-10-12 15:05:43 +0800

    new

---
+++ fujin8/src/fujin8/btfactory/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+# I put this on all required fields, because it's easier to pick up
+# on them with CSS or JavaScript if they have a class of "required"
+# in the HTML. Your mileage may vary. If/when Django ticket #3515
+# lands in trunk, this will no longer be necessary.
+attrs_dict = { 'class': 'required' }
+
+
+class ActressSearchForm(forms.Form):
+    actress = forms.CharField(max_length=30)
